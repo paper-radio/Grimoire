a2c552b1589148f48e0ea98bf4801f2e7f50d644
Alexander Adrian Guerra <alex@paper-rad.io>
2018-07-16 09:17:31 -0500

    Reset

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import Layout from "../../components/Layout/Layout";
-import WeightTracker from "../WeightTracker/WeightTracker";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./App.css";
@@ -9,9 +7,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Layout>
-                    <WeightTracker />
-                </Layout>
+                <Header />
+                <Footer />
             </div>
         );
     }
